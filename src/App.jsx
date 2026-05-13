aa5e7273d1888c1a8d10be711cf37a6aa6c4f370
MEHEDI HASSAN JIBON <mjjibon114@gmail.com>
2026-05-13 12:03:18 +0600

    remove scroll-bar-indicator

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 import Home from "./pages/Home";
-import ScrollBarIndicator from "scroll-bar-indicator";
 
 const App = () => {
   return (
     <div>
-      <ScrollBarIndicator
-        barColor="#b3b3b3"
-        barHeight="2rem"
-        indicatorColor="#8e32ff"
-        infoColor="#333333"
-        zindex="9999"
-        showInfo={true}
-        info="Page"
-      />
       <Home />
     </div>
   );
